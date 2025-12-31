8295acb2e9e9e682d88b2464e5f27cf70953a253
farhanfadhilahedts <farhan.fadhilah@sg-edts.com>
2025-12-31 10:08:56 +0700

    update: access

--- unit_test/test.sh
+++ unit_test/test.sh
@@ -2,4 +2,4 @@ curl -X POST \
   -H "Authorization: Bearer test123" \
   -H "Content-Type: application/json" \
   -d @unit_test/sample_req.json \
-  https://asia-southeast2-my-project.cloudfunctions.net/pubsub-elt-gateway
+  https://us-central1-personal-project-dev-482704.cloudfunctions.net/streaming_gateway
